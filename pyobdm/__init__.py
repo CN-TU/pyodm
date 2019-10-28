3766b2f9d23812df25b732e65dbad39059546403
fm94 <fares.meghdouri@tuwien.ac.at>
2019-10-28 14:33:16 +0100

    adapt the script to paper definitions

--- pyobdm/__init__.py
+++ pyobdm/__init__.py
@@ -5,7 +5,7 @@
 # Copyright (C) 2019, Institute of Telecommunications, TU Wien
 #
 # Name        : __init__.py
-# Description : Data Modeling and Compression algorithm
+# Description : Observers Based Data Modeling algorithm
 # Author      : Fares Meghdouri
 #
 # Notes : 
@@ -13,30 +13,22 @@
 #
 #******************************************************************************
 
-import pandas as pd
 import numpy as np
-
-from tqdm import tqdm
-from sklearn.neighbors import KDTree
 from scipy.spatial import cKDTree
 
-#%matplotlib inline
-import matplotlib.pyplot as plt
-
-class DMC(object):
+class OBDM(object):
     """Data Modeling and Compression"""
-    # TODO: implement different distances, currently DMC supports only the eucledian distance.
     
-    def __init__(self, threshold=None, O=5, factor=0.1, contamination=0.1, n_points=100, random_state=None, chunksize=None, progress_bar=True):
+    def __init__(self, R=None, O=5, factor=0.1, contamination=0.1, n_points=100, random_state=None, chunksize=None, shuffle=False):
         """
         Parameters
         ----------
-        threshold: float, optional
+        R: float, optional
             Default new cluster radius.
             If None, the default radius is estimated based on the mean distance of a set of randomely chosen points
             
         O: int, optional (default=1)
-            Number of closest centroides to use for estimating the outlierness.
+            Number of closest observers to use for estimating the outlierness.
             
         factor: float, optional (default=0.1)
             A factor by wich the radius of a cluster is adjusted.
@@ -47,8 +39,8 @@ class DMC(object):
         n_points: int, optional (default=100)
             Number of data points to use for estimating the initial radius of a new cluster.
             
-        progress_bar: bool, optional (default=True)
-            Use a tqdm progress bar to observe the progress when dealing with large data.
+        shuffle: bool, optional (default=False)
+            Shuffle the input data, this is suited for static data.
             
         random_state: RandomState, int or None, optional (default=None)
             If np.RandomState, use random_state directly. If int, use
@@ -60,22 +52,22 @@ class DMC(object):
             If None, the whole data is taken at once. 
         """
         
-        if threshold:
-            self.estim_threshold = False
-            self.threshold = threshold
+        if R:
+            self.estim_R = False
+            self.R = R
         else:
-            self.estim_threshold = True
-            self.threshold = 'dynamic'
+            self.estim_R = True
+            self.R = 'dynamic'
             
         self.O = O
         self.factor = factor
         self.random_state = random_state
         self.n_points = n_points
         self.chunksize = chunksize
-        self.progress_bar = not progress_bar
+        self.shuffle = shuffle
         self.contamination = contamination
         
-        return print('Initiating a DMC. Threshold = {}, 0 = {} and factor = {}'.format(self.threshold, self.O, self.factor))
+        return
 
     def fit(self, X):
         """
@@ -83,76 +75,67 @@ class DMC(object):
 
         Parameters
         ---------------
-        X: pandas DataFrame, shape (n_samples, n_features)
+        X: ndarray, shape (n_samples, n_features)
             The input data.
         """
 
-        X = X.sample(frac=1, random_state=self.random_state).reset_index(drop=True).values        
+        np.random.seed(self.random_state)
+        if self.shuffle:
+            np.random.shuffle(X)
+            
         [self.m, self.n] = X.shape
         
-        self.centroides = np.empty((1, self.n))
-        self.counts = np.empty(1)
+        self.observers = np.empty((1, self.n))
+        self.population = np.empty(1)
         self.radius = np.empty(1)
         
-        if self.estim_threshold:
-            # either take 100% or 50%
-            self.threshold = self.estimate_threshold(X)/2
-            print('Estimated threshold = {}'.format(self.threshold))
+        if self.estim_R:
+            alpha = 0.5 #to be adjusted
+            self.R = self.estimate_R(X)*alpha
         
         if not self.chunksize:
             self.chunksize = self.m
 
-        for batch_n, batch in enumerate(tqdm(np.array_split(X, self.m/self.chunksize), disable=self.progress_bar)):
+        for batch_n, batch in enumerate(np.array_split(X, self.m/self.chunksize)):
             
             [self.mm, self.nn] = batch.shape
             shift = self.mm*batch_n
             
-            sub_counts = np.zeros([self.mm, 1])
-            sub_centroides = np.empty([self.mm, self.nn])
-            sub_centroides[0] = batch[0]
-            sub_counts[0] += 1
-            sub_radius = np.full([self.mm, 1], self.threshold)
+            sub_population = np.zeros([self.mm, 1])
+            sub_observers = np.empty([self.mm, self.nn])
+            sub_observers[0] = batch[0]
+            sub_population[0] += 1
+            sub_radius = np.full([self.mm, 1], self.R)
             
             i = 0
             
             for index, data_point in enumerate(batch[1:]):
 
-                found = False
-                tree = cKDTree(sub_centroides[:i+1])
+                self.tree = cKDTree(sub_observers[:i+1])
                 
-                dist, ind = tree.query(data_point.reshape(1, -1), k=1)
+                dist, ind = self.tree.query(data_point.reshape(1, -1), k=1)
 
                 if dist <= sub_radius[ind]:
-                    # TODO: check not only the nearest one...
-                    distances = np.subtract(data_point, sub_centroides[ind])
-                    sub_centroides[ind] = sub_centroides[ind] + distances/(sub_counts[ind] + 1)
-                    sub_counts[ind] +=1
-                    sub_radius[ind] = sub_radius[ind] - self.factor * dist#np.linalg.norm(distances)
-                    found = True
+                    distances = np.subtract(data_point, sub_observers[ind])
+                    sub_observers[ind] = sub_observers[ind] + distances/(sub_population[ind] + 1)
+                    sub_population[ind] +=1
+                    sub_radius[ind] = max(0, sub_radius[ind] - self.factor * dist)
                 else:
-                    sub_radius[ind] = sub_radius[ind] + self.factor * dist#np.linalg.norm(distances)
-        
-                if not found:
+                    sub_radius[ind] = max(0, sub_radius[ind] + self.factor * dist)
                     i +=1
-                    sub_centroides[i] = data_point
-                    sub_counts[i] += 1
-                    sub_radius[i] = self.threshold
-                
-                #sub_centroides = np.delete(sub_centroides, list(set(to_delete)), 0)
-                #plt.scatter(sub_centroides[:i,0], sub_centroides[:i,1])
-                #display.clear_output(wait=True)
-                #display.display(pl.gcf())
-                #time.sleep(0.01)
+                    sub_observers[i] = data_point
+                    sub_population[i] += 1
+                    sub_radius[i] = self.R
     
-            self.centroides = np.vstack((self.centroides, sub_centroides[:i+1]))
-            self.counts = np.vstack((self.counts, sub_counts[:i+1]))
+            self.observers = np.vstack((self.observers, sub_observers[:i+1]))
+            self.population = np.vstack((self.population, sub_population[:i+1]))
             self.radius = np.vstack((self.radius, sub_radius[:i+1]))
             
-        self.centroides = np.delete(self.centroides, 0, axis=0)
-        self.counts = np.delete(self.counts, 0, axis=0)
+        self.observers = np.delete(self.observers, 0, axis=0)
+        self.population = np.delete(self.population, 0, axis=0)
         self.radius = np.delete(self.radius, 0, axis=0)
             
-        return self.centroides
+        return
 
     def predict(self, X, mode='median'):
         """
@@ -168,11 +151,14 @@ class DMC(object):
             
         Returns
         ---------------
-        y: pandas DataFrame, shape (n_samples)
+        y: ndarray, shape (n_samples)
             Binary predicted labels for input data.
         """
         
-        #X = X.values    
+        np.random.seed(self.random_state)
+        if self.shuffle:
+            np.random.shuffle(X)
+            
         outlierness = self.outlierness(X, mode=mode)
         threshold = np.quantile(outlierness, 1-self.contamination)
         return outlierness > threshold
@@ -183,7 +169,7 @@ class DMC(object):
         
         Parameters
         ---------------
-        X: pandas DataFrame, shape (n_samples, n_features)
+        X: ndarray, shape (n_samples, n_features)
             The input data.
         
         mode: str, [median (default), 'mean', 'max', 'min', 'sum']
@@ -195,12 +181,14 @@ class DMC(object):
             Outlier scores for input data.
         """
         
-        X = X.values    
+        np.random.seed(self.random_state)
+        if self.shuffle:
+            np.random.shuffle(X)
+            
         out = np.empty(self.m)
-        tree = cKDTree(self.centroides)
         
         for index, data_point in enumerate(X):
-            dist, ind = tree.query(data_point.reshape(1, -1), k=self.O)
+            dist, ind = self.tree.query(data_point.reshape(1, -1), k=self.O)
             
             if mode == 'mean':
                 out[index] = np.mean(dist)
@@ -215,7 +203,7 @@ class DMC(object):
 
         return out
 
-    def estimate_threshold(self, X):
+    def estimate_R(self, X):
         """
         Estimate the average disatnce between points which will be used as initial new cluster radius.
         The distance is estimated by taking a random (random_state) subset of points (n_points) and average the distance btween them.
@@ -227,19 +215,35 @@ class DMC(object):
             
         Returns
         ---------------
-        threshold: float
+        R: float
             The estimnated default radius.
         """
         
-        #X = X.values
-        np.random.seed(self.random_state)
         data = X[np.random.choice(self.m, self.n_points, replace=False), :]
         tot = 0.
         for i in range(self.n_points-1):
             tot += ((((data[i+1:]-data[i])**2).sum(1))**.5).sum()
             
         return tot/((self.n_points-1)*(self.n_points)/2.)
-    
+
+    def labels(self, X):
+        """
+        Return the label of each data point based on the closest observer
+        
+        Parameters
+        ---------------
+        X: ndarray, shape (n_samples, n_features)
+            The input data.
+            
+        Returns
+        ---------------
+        labels: ndarray, shape (n_samples)
+            Outlier scores for input data.
+        """
+            
+        dist, ind = self.tree.query(X, k=1)
+        return ind
+        
     def get_params(self, deep=True):
         """
         Return the model's parameters
@@ -255,12 +259,12 @@ class DMC(object):
             A dictionnary mapping of the model's parameters.
         """
         
-        return {"threshold":self.threshold,
+        return {"R":self.R,
                 "O":self.O,
                 "factor":self.factor,
                 "random_state":str(self.random_state),
                 "n_points":self.n_points,
                 "chunksize":self.chunksize,
                 "progress_bar":self.progress_bar,
-                "contamination":self.contamination}
-    
+                "contamination":self.contamination,
+                "shuffle":self.shuffle}
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-	name='pydmc',
-	version='1.1',
-	description='DMC Data Modeling and Compression',
+	name='pyobdm',
+	version='1.2',
+	description='OBDM Observers Based Data Modeling',
 	author='Fares Meghdouri',
 	author_email='fares.meghdouri@tuwien.ac.at',
-	url='https://github.com/CN-TU/pydmc',
-	packages=['pydmc'],
-	requires=['numpy', 'scikit_learn', 'scipy', 'pandas', 'matplotlib']
+	url='https://github.com/CN-TU/pyobdm',
+	packages=['pyobdm'],
+	requires=['numpy',  'scipy']
 )
